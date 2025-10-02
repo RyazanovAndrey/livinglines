3eaf6ca294638f3a419d86449c9dd7ce1982a932
RyazanovAndrey <ryazanov.andrej@gmail.com>
2025-10-02 10:35:49 +0300

    10.35

--- src/data.ts
+++ src/data.ts
@@ -7,9 +7,9 @@ export const navLinks: { title: string, href: string}[] = [
 ]
 
 export const sliderList: { desc: string, title: string, btnTitle: string, image: string, content: string }[] = [
-    { desc: 'Furniture Selection', title: 'Design Perfection', btnTitle: 'Portfolio', image: '/images/Hero-3.jpg', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam quas saepe optio eum odit esse consequuntur praesentium laudantium natus dolores!' },
-    { desc: 'Lighting Solutions', title: 'Elegant Living', btnTitle: 'Blog', image: '/images/Hero-2.jpg', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam quas saepe optio eum odit esse consequuntur praesentium laudantium natus dolores!' },
-    { desc: 'Color Harmony', title: 'Functional Beauty', btnTitle: 'Services', image: '/images/Hero-1.jpg', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam quas saepe optio eum odit esse consequuntur praesentium laudantium natus dolores!' }
+    { desc: 'Furniture Selection', title: 'Design Perfection', btnTitle: 'Portfolio', image: '/livinglines/images/Hero-3.jpg', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam quas saepe optio eum odit esse consequuntur praesentium laudantium natus dolores!' },
+    { desc: 'Lighting Solutions', title: 'Elegant Living', btnTitle: 'Blog', image: '/livinglines/images/Hero-2.jpg', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam quas saepe optio eum odit esse consequuntur praesentium laudantium natus dolores!' },
+    { desc: 'Color Harmony', title: 'Functional Beauty', btnTitle: 'Services', image: '/livinglines/images/Hero-1.jpg', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam quas saepe optio eum odit esse consequuntur praesentium laudantium natus dolores!' }
 ]
 
 export const lineWords: string[] = [
@@ -24,36 +24,36 @@ export const lineWords: string[] = [
     'furniture & decor']
 
 export const sliderImg: string[] = [
-    '/images/interior-image-01.jpg',
-    '/images/interior-image-02.jpg',
-    '/images/interior-image-03.jpg',
-    '/images/interior-image-05.jpg',
-    '/images/interior-image-06.jpg'
+    '/livinglines/images/interior-image-01.jpg',
+    '/livinglines/images/interior-image-02.jpg',
+    '/livinglines/images/interior-image-03.jpg',
+    '/livinglines/images/interior-image-05.jpg',
+    '/livinglines/images/interior-image-06.jpg'
 ]
 
 export const propcessList: { title: string, image: string, desc: string }[] = [
-    { title: 'Consultation and initial meeting', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.', image: '/images/process-icon-1.svg' },
-    { title: 'Concept design', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.', image: '/images/process-icon-2.svg' },
-    { title: 'Design development', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.', image: '/images/process-icon-3.svg' },
-    { title: 'Permitting and approvals', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.', image: '/images/process-icon-4.svg' },
+    { title: 'Consultation and initial meeting', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.', image: '/livinglines/images/process-icon-1.svg' },
+    { title: 'Concept design', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.', image: '/livinglines/images/process-icon-2.svg' },
+    { title: 'Design development', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.', image: '/livinglines/images/process-icon-3.svg' },
+    { title: 'Permitting and approvals', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.', image: '/livinglines/images/process-icon-4.svg' },
 ]
 
 export const solutionList = [
-    { image: '/images/service-icon-1.svg', title: 'Furniture Selection', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
-    { image: '/images/service-icon-2.svg', title: 'Color Consultation', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
-    { image: '/images/service-icon-3.svg', title: 'Lighting Design', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
-    { image: '/images/service-icon-4.svg', title: 'Wall Treatment', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
-    { image: '/images/service-icon-5.svg', title: 'Flooring Design', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
-    { image: '/images/service-icon-6.svg', title: 'Kitchens', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
+    { image: '/livinglines/images/service-icon-1.svg', title: 'Furniture Selection', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
+    { image: '/livinglines/images/service-icon-2.svg', title: 'Color Consultation', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
+    { image: '/livinglines/images/service-icon-3.svg', title: 'Lighting Design', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
+    { image: '/livinglines/images/service-icon-4.svg', title: 'Wall Treatment', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
+    { image: '/livinglines/images/service-icon-5.svg', title: 'Flooring Design', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
+    { image: '/livinglines/images/service-icon-6.svg', title: 'Kitchens', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
 ]
 
 export const architectureList = [
-    { image: '/images/structure-design-icon.png', title: 'Structure Design', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
-    { image: '/images/residential-area-icon.png', title: 'Residential Space', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
-    { image: '/images/office-icon.png', title: 'Commercial', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
-    { image: '/images/landscape-icon.png', title: 'Landscape', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
-    { image: '/images/Renovation-icon.png', title: 'Renovation', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
-    { image: '/images/service-icon-1.svg', title: 'Space Planning', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
+    { image: '/livinglines/images/structure-design-icon.png', title: 'Structure Design', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
+    { image: '/livinglines/images/residential-area-icon.png', title: 'Residential Space', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
+    { image: '/livinglines/images/office-icon.png', title: 'Commercial', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
+    { image: '/livinglines/images/landscape-icon.png', title: 'Landscape', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
+    { image: '/livinglines/images/Renovation-icon.png', title: 'Renovation', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
+    { image: '/livinglines/images/service-icon-1.svg', title: 'Space Planning', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, provident.' },
 ]
 
 export const testList: { content: string, name: string, city: string }[] = [
